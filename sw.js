7d2d84840ca4ff303d965c9a2b89136ca78c48b7
naricci5089 <naricci5089@gmail.com>
2026-06-18 19:31:31 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='todo-v1';
+const FILES=['/','index.html'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
